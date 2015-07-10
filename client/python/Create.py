27f74e29a1a6c66e9c9fb4872a2e23d834904fd3
Van Kichline <vkichline@hotmail.com>
2015-07-09 20:21:51 -0700

    Added incomplete but usable Python client files.

---
+++ client/python/Create.py
@@ -0,0 +1,24 @@
+import httplib
+
+def api_create(data):
+    data = '{"Content":"' + data + '"}';
+    conn = httplib.HTTPConnection("localhost", 47514)
+    conn.connect()
+    request = conn.putrequest('POST', '/api/items')
+    headers = {}
+    headers['Content-Type'] = 'application/json'
+    headers['Content-Length'] = len(data)
+    for k in headers:
+        conn.putheader(k, headers[k])
+    conn.endheaders()
+
+    conn.send(data)
+    resp = conn.getresponse()
+    if 201 == resp.status:
+        loc = resp.getheader("location")
+        print "SUCCESS: Location =", loc
+    else:
+        print "ERROR:", resp.status, resp.reason
+    conn.close()
+
+api_create("Testing 1, 2, 3")
---
+++ client/python/Read.py
@@ -0,0 +1,16 @@
+﻿import httplib
+
+def api_read():
+    conn = httplib.HTTPConnection("localhost", 47514)
+    conn.request("GET", "/api/items")
+    resp = conn.getresponse()
+    if 200 == resp.status:
+        data = resp.read()
+        return "Response:", data
+    else:
+        return "ERROR:", resp.status, resp.reason
+
+print api_read()
+
+# Do I need conn.close()?  where?
+# Need GetById
